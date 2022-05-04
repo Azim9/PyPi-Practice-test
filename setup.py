ae25d7eed064e989536592113492b91e00f1cdaf
Azim9 <68808693+Azim9@users.noreply.github.com>
2022-05-04 10:49:06 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name='PyPiTest123',
+  version='0.0.1',
+  descripiton='oss-dev class PyPi test output',
+  author='Azimjon Mirzoev',
+  ulr='https://github.com/Azim9/PyPi-Practice-test/new/main',
+  download_url='https://github.ocm/Azim(/PyPi-Practice-test',
+  isntall_requires=['pytest'],
+  long_description_content_type='text/markdown',
+  classifiers=[
+    'Programming Language:: Python :: 3',
+    'Operating System :: OS Independent',
+  ],
+)
